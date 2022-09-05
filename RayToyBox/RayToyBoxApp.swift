1f813e00ec4a8b115e62d9d5d9ba7ea563ffe3c5
RayTjan <rayy0001@student.ciputra.ac.id>
2022-09-05 17:32:35 +0700

    Initial commit

---
+++ RayToyBox/RayToyBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  RayToyBoxApp.swift
+//  RayToyBox
+//
+//  Created by Ray on 30/08/22.
+//
+
+import SwiftUI
+
+@main
+struct RayToyBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
